ddc4acda0e2fa0d5da432d4764ac9599f95044ec
sanobar28 <sanobar.mujawar@gmail.com>
2021-05-31 15:46:08 +0530

    added: random function to check present absent

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,7 +1,16 @@
 public class EmployeeWage{
 
 	public static void main(String[] args){
-	
+		
+		
 		System.out.println("Welcome to employee wage computation program");
+	
+		int IS_FULL_TIME=1;
+		
+		double empcheck = Math.floor(Math.random()*10)%2;
+		if (empcheck == IS_FULL_TIME)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is not present");
 	}
 }
