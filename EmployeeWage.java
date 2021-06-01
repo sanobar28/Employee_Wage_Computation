93c0c5ac3cb7d38636786960a0030ee3fb4a2269
sanobar28 <sanobar.mujawar@gmail.com>
2021-06-01 15:36:55 +0530

    added: Total emp Wage calculated using while loop

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,18 +4,23 @@ public class EmployeeWage{
 	public static final int IS_PART_TIME=2;
 	public static final int EMP_RATE_PER_HOUR=20;
 	public static final int NUM_OF_WORK_DAYS=20;
+	public static final int MAX_HRS_IN_MONTH=100;
 
 
 	public static void main(String[] args){
 		
 
-		System.out.println("Welcome to employee wage computation program");
+		//System.out.println("Welcome to employee wage computation program");
 	
-		int empHrs=0, empWage=0, totalEmpWage=0;
+		int empHrs=0, empWage=0,
+		totalEmpWage=0, totalWorkingHrs=0, totalWorkdays=0;
 		
-		int empcheck = (int) Math.floor(Math.random()*10)%3;
 		
-		for (int day=1; day <= NUM_OF_WORK_DAYS; day++){
+		while (totalWorkingHrs <= MAX_HRS_IN_MONTH && 
+		totalWorkdays < NUM_OF_WORK_DAYS){
+			
+			totalWorkdays++;
+			int empcheck = (int) Math.floor(Math.random()*10)%3;
 			
 			switch (empcheck){
 				
@@ -31,12 +36,15 @@ public class EmployeeWage{
 					empHrs=0;
 			}
 			
+			totalWorkingHrs += empHrs;
 			empWage= empHrs * EMP_RATE_PER_HOUR;
 			totalEmpWage += empWage;
 		}
 		
 		
 		//System.out.println("Emp Wage per day: " + empWage);
-		System.out.println("Emp Wage for month: " + totalEmpWage);
+		System.out.println("Total working hours : " + totalWorkingHrs);
+		System.out.println("No. of working days: " + totalWorkdays);
+		System.out.println("Total Emp Wage for month: " + totalEmpWage);
 	}
 }
