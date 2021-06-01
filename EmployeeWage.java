97a09bf6f32bcd3c514d7727c84ecf87599d1753
sanobar28 <sanobar.mujawar@gmail.com>
2021-06-01 12:17:24 +0530

    added: emp Wage per month

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,6 +3,7 @@ public class EmployeeWage{
 	public static final int IS_FULL_TIME=1;
 	public static final int IS_PART_TIME=2;
 	public static final int EMP_RATE_PER_HOUR=20;
+	public static final int NUM_OF_WORK_DAYS=20;
 
 
 	public static void main(String[] args){
@@ -10,26 +11,32 @@ public class EmployeeWage{
 
 		System.out.println("Welcome to employee wage computation program");
 	
-		int empHrs=0, empWage=0;
+		int empHrs=0, empWage=0, totalEmpWage=0;
 		
 		int empcheck = (int) Math.floor(Math.random()*10)%3;
 		
-		switch (empcheck){
+		for (int day=1; day <= NUM_OF_WORK_DAYS; day++){
 			
-			case(IS_FULL_TIME):
-				empHrs=8;
-				break;		
-		
-			case(IS_PART_TIME):
-				empHrs=4;
-				break;
-		
-			default:
-				empHrs=0;
+			switch (empcheck){
+				
+				case(IS_FULL_TIME):
+					empHrs=8;
+					break;		
+			
+				case(IS_PART_TIME):
+					empHrs=4;
+					break;
+			
+				default:
+					empHrs=0;
+			}
+			
+			empWage= empHrs * EMP_RATE_PER_HOUR;
+			totalEmpWage += empWage;
 		}
 		
-		empWage= empHrs * EMP_RATE_PER_HOUR;
 		
-		System.out.println("Emp Wage per day: " + empWage);
+		//System.out.println("Emp Wage per day: " + empWage);
+		System.out.println("Emp Wage for month: " + totalEmpWage);
 	}
 }
