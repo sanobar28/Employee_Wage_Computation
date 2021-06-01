71273be8a0b96a30023dbc64ed695e77219328d5
sanobar28 <sanobar.mujawar@gmail.com>
2021-06-01 11:37:38 +0530

    added: emp Wage Per Day calculation

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,16 +1,29 @@
 public class EmployeeWage{
+	
+	public static final int IS_FULL_TIME=1;
+	public static final int IS_PART_TIME=2;
+	public static final int EMP_RATE_PER_HOUR=20;
+
 
 	public static void main(String[] args){
 		
-		
+
 		System.out.println("Welcome to employee wage computation program");
 	
-		int IS_FULL_TIME=1;
+		int empHrs=0, empWage=0;
 		
-		double empcheck = Math.floor(Math.random()*10)%2;
-		if (empcheck == IS_FULL_TIME)
+		int empcheck = (int) Math.floor(Math.random()*10)%2;
+		
+		if (empcheck == IS_FULL_TIME){
 			System.out.println("Employee is present");
-		else
+			empHrs=8;
+		}
+		else{
 			System.out.println("Employee is not present");
+			empHrs=0;
+		}
+		empWage= empHrs * EMP_RATE_PER_HOUR;
+		
+		System.out.println("Emp Wage per day" + empWage);
 	}
 }
